e32e854c3a13bfe16ef9f552284099a41952aefd
Herkata <86102368+Herkata@users.noreply.github.com>
2024-02-17 14:18:49 +0100

    Add files via upload

---
+++ cfg-proj.js
@@ -0,0 +1,32 @@
+function playSound(element) {
+
+}
+
+function goTo(){
+
+}
+
+function setAccessibility(){
+
+}
+
+function toggleMute(){
+
+}
+
+function resize(){
+}
+
+function buttonClick(){ 
+}
+
+function PlaySound(soundobj) {
+    var thissound = document.getElementById(soundobj);
+    thissound.play();
+  }
+  
+  function StopSound(soundobj) {
+    var thissound = document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+  }
